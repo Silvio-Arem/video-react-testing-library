7b756ff771ffc6467890e624db6a83bb598d132f
Silvio Arem <silvio.arem@gmail.com>
2023-02-09 12:16:09 -0300

    feat: criada a função que vai pegar a task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 const Todo = () => {
     const [task, updateTask] = useState('')
+    const [tasks, updateTasks] = useState([]);
 
     // Criando uma função para o recebimento do campo do form
     const handleInputChange = event => updateTask(event.target.value)
 
+    const handleFormSubmit = event =>{
+        event.preventDefault()
+        updateTasks([...tasks, task])
+        updateTask('')
+    }
+
     // Criando um retorno com campos de formulário
     return (
         <>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <input onChange={handleInputChange} placeholder="Type a new task here" type="text" value={task}/>
                 <button type="submit">Add new task</button>
             </form>
